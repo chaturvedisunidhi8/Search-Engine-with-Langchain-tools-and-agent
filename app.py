1eb642c4a3a5506a8475d029a17ecf1a003cc9f2
Sunidhi Chaturvedi <chaturvedisunidhi8@gmail.com>
2026-05-19 13:07:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,6 @@ if (prompt := st.chat_input("Ask something...")):
     # Stream and display assistant reply 
     with st.chat_message("assistant"):
         st_cb = StreamlitCallbackHandler(st.container())    
-        response = search_agent.run(prompt, callbacks=[st_cb])     pass prompt
+        response = search_agent.run(prompt, callbacks=[st_cb])     ass prompt
         st.session_state.messages.append({"role": "assistant", "content": response})
         st.write(response)
